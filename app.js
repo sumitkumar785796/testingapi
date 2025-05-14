bdf0bbd50b726961ab48932ea0d23dbcb5ec6e0e
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2025-05-14 13:32:13 +0530

    change url1

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://testingfrontsumit.vercel.app/',
+    origin: 'https://testingfrontsumit.vercel.app',
     credentials: true
 }));  
 app.use("/api/users", userroutes)
